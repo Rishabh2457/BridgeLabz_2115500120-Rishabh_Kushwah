7f77182d6e6a397d741c89f39a3ed1e7a2bab695
Rishabh2457 <rishabh.kushwah_cs.aiml21@gla.ac.in>
2025-01-19 22:04:58 +0530

    Adding BridgelabzWelcome program

---
+++ BridgeWel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BridgelabzWelcome
+{
+    class BridgeWel
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bridgelabz!");
+        }
+    }
+}
