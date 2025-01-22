425528fecc0ad56eecd7cf7b7b605c7c0888aa84
Rishabh2457 <rishabh.kushwah_cs.aiml21@gla.ac.in>
2025-01-22 23:33:21 +0530

    Question 3 solution

---
+++ DistoMiles.cs
@@ -0,0 +1,13 @@
+using System;
+
+class DistoMiles{
+	public static void Main(string[] args)
+	{
+		//1 km  = 1.6 miles
+		double kM = 10.8;
+		double miles = kM*1.6;
+		Console.WriteLine(string.Format("The distance {0}km in miles is {1}",kM,miles));
+		
+		}
+	
+	}
